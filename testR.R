ba4a33614d03bdc5589357b83467f6568905ab91
monicarva <m.carvajal@cgiar.org>
2018-12-06 22:50:44 -0500

    update

--- testR.R
+++ testR.R
@@ -3,3 +3,13 @@ delta (iris)
 head(iris)
 
 getwd()
+
+#create a repository with a code
+#go to github and create a new repository
+
+name(name)
+
+#continue with copying the URL adn go to RStudio
+
+# press 
+file > new project > version control > git > paste()
